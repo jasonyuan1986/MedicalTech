65eed1b618a3d4e38c2c213690ad17525c75264b
袁鑫 <Jason@jasonsmacbook-pro.local>
2016-07-01 14:18:33 +0800

    Initial Commit

---
+++ MedicalTech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MedicalTech
+//
+//  Created by Jason on 7/1/16.
+//  Copyright © 2016 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MedicalTech/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MedicalTech
+//
+//  Created by Jason on 7/1/16.
+//  Copyright © 2016 Jason. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
